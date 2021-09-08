3f6791539dfb5979ceb96a94c5227a1b5e2ef335
KPAHAPK <Roman444.zaharov@gmail.com>
2021-09-08 12:45:07 +0300

    tried CustomImageView

--- app/src/main/java/com/example/nasaapp/view/MainActivity.kt
+++ app/src/main/java/com/example/nasaapp/view/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.example.nasaapp
+package com.example.nasaapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nasaapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
