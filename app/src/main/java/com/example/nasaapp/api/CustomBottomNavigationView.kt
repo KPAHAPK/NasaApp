f313a51cbdb0eef58fb03b284013c0163868692e
KPAHAPK <Roman444.zaharov@gmail.com>
2021-09-22 13:26:41 +0300

    constraintSet

--- app/src/main/java/com/example/nasaapp/api/CustomBottomNavigationView.kt
+++ app/src/main/java/com/example/nasaapp/api/CustomBottomNavigationView.kt
@@ -1,4 +1,17 @@
 package com.example.nasaapp.api
 
-class CustomBottomNavigationView @JvmOverloads constructor() {
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class CustomBottomNavigationView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : BottomNavigationView(context, attrs, defStyleAttr, defStyleRes) {
+
+    companion object {
+        const val MAX_ITEM_COUNT = 10
+    }
 }
---
+++ app/src/main/java/com/example/nasaapp/view/animation/AnimationActivityBonus.kt
@@ -0,0 +1,51 @@
+package com.example.nasaapp.view.animation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.transition.ChangeBounds
+import android.transition.TransitionManager
+import android.view.animation.AnticipateOvershootInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.example.nasaapp.R
+import com.example.nasaapp.databinding.ActivityAnimationBonusStartBinding
+
+class AnimationActivityBonus: AppCompatActivity() {
+
+    private lateinit var binding: ActivityAnimationBonusStartBinding
+
+    private var show = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAnimationBonusStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backgroundImage.setOnClickListener{
+            val constraintSet = ConstraintSet()
+
+            val transition = ChangeBounds()
+            show = !show
+            if (show) {
+                constraintSet.clone(this, R.layout.activity_animation_bonus_end)
+                transition.interpolator = AnticipateOvershootInterpolator( 2.0f)
+                transition.duration = 600
+            }else {
+                constraintSet.clone(this, R.layout.activity_animation_bonus_start)
+                transition.interpolator = AnticipateOvershootInterpolator(2.0f)
+                transition.duration = 600
+            }
+            TransitionManager.beginDelayedTransition(binding.constraintContainer, transition)
+            constraintSet.applyTo(binding.constraintContainer )
+        }
+
+
+
+
+
+
+
+    }
+}
