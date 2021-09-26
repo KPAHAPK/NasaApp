fca93ed74acfebf57ee92f5c78bd935fcda02e6a
KPAHAPK <Roman444.zaharov@gmail.com>
2021-09-26 20:38:02 +0300

    Введены действия над Items

--- app/src/main/java/com/example/nasaapp/view/recycler/RecyclerActivity.kt
+++ app/src/main/java/com/example/nasaapp/view/recycler/RecyclerActivity.kt
@@ -7,28 +7,30 @@ import com.example.nasaapp.databinding.ActivityRecyclerBinding
 
 
 class RecyclerActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityRecyclerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecyclerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val data = mutableListOf<Data>()
-        repeat(10) {
-            if (it % 2 == 0) {
-                data.add(Data("Earth"))
-            } else {
-                data.add(Data("Mars", ""))
-            }
-        }
         data.add(0, Data("Header"))
+        data.add(Data("Footer"))
 
-        binding.recyclerView.adapter = RecyclerActivityAdapter(
+        val adapter = RecyclerActivityAdapter(
             object : OnListItemClickListener {
                 override fun onItemClick(data: Data) {
                     Toast.makeText(this@RecyclerActivity, data.someText, Toast.LENGTH_SHORT).show()
                 }
             }, data
         )
+
+        binding.recyclerView.adapter = adapter
+
+        binding.recyclerActivityFAB.setOnClickListener {
+            adapter.appendItem()
+        }
     }
 }
--- app/src/main/java/com/example/nasaapp/view/recycler/RecyclerActivityAdapter.kt
+++ app/src/main/java/com/example/nasaapp/view/recycler/RecyclerActivityAdapter.kt
@@ -5,12 +5,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nasaapp.databinding.ActivityRecyclerItemEarthBinding
+import com.example.nasaapp.databinding.ActivityRecyclerItemFooterBinding
 import com.example.nasaapp.databinding.ActivityRecyclerItemHeaderBinding
 import com.example.nasaapp.databinding.ActivityRecyclerItemMarsBinding
 
 class RecyclerActivityAdapter(
     private var onListItemClickListener: OnListItemClickListener,
-    private var data: List<Data>
+    private var data: MutableList<Data>
 ) : RecyclerView.Adapter<BaseViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
@@ -43,20 +44,32 @@ class RecyclerActivityAdapter(
                 HeaderViewHolder(binding.root)
             }
 
+            TYPE_FOOTER -> {
+                val binding: ActivityRecyclerItemFooterBinding =
+                    ActivityRecyclerItemFooterBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                    )
+                FooterViewHolder(binding.root)
+            }
+
             else -> throw IllegalStateException("Invalid ViewType")
 
         }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if (position == 0) TYPE_HEADER else {
-            when (data[position].someDescription.isBlank()) {
-                true -> TYPE_MARS
-                else -> TYPE_EARTH
-
+        return when (position) {
+            0 -> TYPE_HEADER
+            data.lastIndex -> TYPE_FOOTER
+            else -> {
+                when (data[position].someDescription.isBlank()) {
+                    true -> TYPE_MARS
+                    else -> TYPE_EARTH
+                }
             }
         }
     }
+
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         (holder).bind(data[position])
@@ -80,10 +93,54 @@ class RecyclerActivityAdapter(
     inner class MarsViewHolder(view: View) : BaseViewHolder(view) {
         override fun bind(data: Data) {
             ActivityRecyclerItemMarsBinding.bind(itemView).apply {
+                marsTextView.text = data.someText
                 marsImageView.setOnClickListener {
                     onListItemClickListener.onItemClick(data)
                 }
+                addItemImageView.setOnClickListener {
+                    addItem()
+                }
+                removeItemImageView.setOnClickListener {
+                    removeItem()
+                }
+                moveItemUp.setOnClickListener {
+                    moveUp()
+                }
+
+                moveItemDown.setOnClickListener {
+                    moveDown()
+                }
             }
+        }
+
+        private fun moveUp() {
+            layoutPosition.takeIf { it > 1 }?.also { currentPosition ->
+                data.removeAt(currentPosition).apply {
+                    data.add(currentPosition - 1, this)
+                }
+                notifyItemMoved(currentPosition, currentPosition - 1)
+            }
+        }
+
+        private fun moveDown() {
+            layoutPosition.takeIf { it < itemCount - 2 }?.also { currentPosition ->
+                data.removeAt(currentPosition).apply {
+                    data.add(currentPosition + 1, this)
+                }
+                notifyItemMoved(currentPosition, currentPosition + 1)
+            }
+        }
+
+        private fun addItem() {
+            data.add(layoutPosition, generateItem())
+            notifyItemInserted(layoutPosition)
+            notifyItemRangeChanged(layoutPosition + 1, data.size - 1)
+        }
+
+        private fun removeItem() {
+            data.removeAt(layoutPosition)
+            notifyItemRemoved(layoutPosition)
+            notifyItemRangeChanged(layoutPosition, data.size - 1)
         }
     }
 
@@ -97,10 +154,39 @@ class RecyclerActivityAdapter(
         }
     }
 
+    inner class FooterViewHolder(view: View) : BaseViewHolder(view) {
+        override fun bind(data: Data) {
+            ActivityRecyclerItemFooterBinding.bind(itemView).apply {
+                footer.setOnClickListener {
+                    onListItemClickListener.onItemClick(data)
+                }
+            }
+        }
+
+    }
+
+    fun appendItem() {
+        val insertPosition = data.lastIndex
+        data.add(insertPosition, generateItem())
+        notifyItemInserted(insertPosition + 1)
+    }
+
+    private var i = 0
+    private fun generateItem(): Data {
+        return Data("Mars ${i++}", "")
+    }
+
+    init {
+        repeat(5) {
+            appendItem()
+        }
+    }
+
     companion object {
         private const val TYPE_EARTH = 0
         private const val TYPE_MARS = 1
         private const val TYPE_HEADER = 2
+        private const val TYPE_FOOTER = 3
     }
 
 
